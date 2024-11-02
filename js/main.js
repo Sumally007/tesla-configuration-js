a9c84247d82985236f0a841a60fa92954153e5e7
iamismail <ismailmbarouk@gmail.com>
2024-11-02 21:30:54 +0300

    adding exterior and interior images

--- js/main.js
+++ js/main.js
@@ -12,6 +12,23 @@ const handleScroll = () => {
     topBar.classList.toggle('hidden-bar', !atTop);
 };
 
+
+// Image Mapping
+
+const exteriorImages = {
+    'Stealth Gray': './images/model-y-stealth-grey.jpg',
+    'Pearl White': './images/model-y-pearl-white.jpg',
+    'Deep Blue': './images/model-y-deep-blue-metallic.jpg',
+    'Solid Black': './images/model-y-solid-black.jpg',
+    'Ultra Red': './images/model-y-ultra-red.jpg',
+    'Quicksilver': './images/model-y-quicksilver.jpg',
+}
+
+const interiorImages = {
+    Dark: './images/model-y-interior-dark.jpg',
+    Light: './images/model-y-interior-light.jpg'
+}
+
 // Handle Color Selection
 const handleColorButtonClick = (event) => {
     let button;
@@ -26,6 +43,19 @@ const handleColorButtonClick = (event) => {
         const buttons = event.currentTarget.querySelectorAll('button');
         buttons.forEach((btn) => btn.classList.remove('btn-selected'));
         button.classList.add('btn-selected');
+
+        // Change exterior image
+
+        if (event.currentTarget === exteriorColorSection){
+            const color = button.querySelector('img').alt;
+            exteriorImage.src = exteriorImages[color];
+        }
+
+        // Change interior image
+        if (event.currentTarget === interiorColorSection) {
+            const color = button.querySelector('img').alt;
+            interiorImage.src = interiorImages[color];
+        }
     }
 };
 
