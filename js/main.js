c661a11e2ef30e0b9d7222c8329cb23f4a78fa1e
iamismail <ismailmbarouk@gmail.com>
2024-11-02 15:23:26 +0300

    adding js for top-bar exterior and interiorColorSection

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const topBar = document.querySelector('#top-bar');
+const exteriorColorSection = document.querySelector('#exterior-buttons');
+const interiorColorSection = document.querySelector('#interior-buttons');
+const exteriorImage = document.querySelector('#exterior-image');
+const interiorImage = document.querySelector('#interior-image');
+
+// Handle Top Bar On Scroll
+
+const handleScroll = () => {
+    const atTop = window.scrollY === 0;
+    topBar.classList.toggle('visible-bar', atTop);
+    topBar.classList.toggle('hidden-bar', !atTop);
+};
+
+// Handle Color Selection
+const handleColorButtonClick = (event) => {
+    let button;
+
+    if (event.target.tagName === 'IMG'){
+        button = event.target.closest('button');
+    } else if (event.target.tagName === 'BUTTON') {
+        button = event.target;
+    }
+
+    if (button) {
+        const buttons = event.currentTarget.querySelectorAll('button');
+        buttons.forEach((btn) => btn.classList.remove('btn-selected'));
+        button.classList.add('btn-selected');
+    }
+};
+
+// Event Listeners
+window.addEventListener('scroll', () => requestAnimationFrame(handleScroll));
+exteriorColorSection.addEventListener('click', handleColorButtonClick);
+interiorColorSection.addEventListener('click', handleColorButtonClick);
